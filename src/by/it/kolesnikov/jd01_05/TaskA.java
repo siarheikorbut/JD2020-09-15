a4e14353dde6135e69ae5a3aed31a94f355cff90
Ghostik13 <roxan95@,ail.ru>
2020-09-24 12:44:45 +0300

    jd01_05 TaskA1 complete

---
+++ src/by/it/kolesnikov/jd01_05/TaskA.java
@@ -0,0 +1,17 @@
+package by.it.kolesnikov.jd01_05;
+import static java.lang.Math.*;
+
+public class TaskA {
+    public static void main(String[] args) {
+        step1();
+    }
+    private static void step1 (){
+        double x=0.3;
+        double a=756.13;
+        double part1=cos(pow(x*x+PI/6, 5));
+        double part2=sqrt(x*pow(a,3));
+        double part3=log(abs((a-1.12*x)/4));
+        double z=part1-part2-part3;
+        System.out.printf("For x=%f and a=%f z=%f\n ", x, a, z);
+    }
+}
