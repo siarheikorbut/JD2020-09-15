677d727bf7bc9b5c05a9fbc951f20f31bb1f1887
igor <gob1in@Mail.ru>
2020-09-24 12:41:19 +0300

    начало задания А1

---
+++ src/by/it/dobrodey/jd01_05/TaskA.java
@@ -0,0 +1,24 @@
+package by.it.dobrodey.jd01_05;
+
+import static java.lang.Math.*;
+
+public class TaskA {
+    public static void main(String[] args) {
+        step1();
+
+    }
+
+    private static void step1() {
+       double x = 0.3;
+       double a = 756.13;
+
+        double part1 = cos((pow(x*x+PI/6,5)));
+        double part2 =
+
+    }
+
+ 
+
+
+
+}
