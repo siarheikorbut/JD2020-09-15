ae0c3b9d95cbd3503f0cb364e239b99da0557933
jmabracodabra <jmabracodabra@gmail.com>
2020-09-24 12:23:39 +0300

    added files form old project

---
+++ src/by/it/frolova/jd01_01/TaskC3.java
@@ -0,0 +1,55 @@
+package by.it.frolova.jd01_01;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+/*
+Ускорение свободного падения на Земле и Марсе таково:
+Марс   3.86
+Земля   9.81
+
+С клавиатуры вводится вес человека в килограммах.
+Рассчитайте вес человека на Марсе и выведите
+округлив его до сотых килограмма (2 знака)
+
+Создайте для этих целей метод getWeight(int weight)
+
+Требования:
+1. Метод getWeight(int weight) должен быть статическим.
+2. Метод getWeight должен возвращать значение типа double.
+3. Метод getWeight должен обязательно (!) округлять до сотых возвращаемое значение типа double.
+4. Метод getWeight не должен ничего выводить на экран.
+5. Метод getWeight должен правильно переводить вес тела в килограммах на Земле
+    в вес этого же тела на Марсе, и возвращать это значение.
+
+Пример:
+
+Ввод:
+75
+
+Вывод:
+29.51
+
+
+*/
+class TaskC3 {
+
+    static double getWeight(int weight) {
+        double pOnMars = weight * 3.86;
+        double mOnEarth = pOnMars / 9.81;
+        BigDecimal bd = new BigDecimal(Double.toString(mOnEarth));
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int weight = sc.nextInt();
+        double result = getWeight(weight);
+        System.out.println(result);
+
+    }
+}
+
+
