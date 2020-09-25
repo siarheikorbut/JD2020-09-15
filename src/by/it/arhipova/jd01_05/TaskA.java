570540ec07babd3ba0fcc3b73eda6fd16982db72
mariarhipova <maria.arhipova92@gmail.com>
2020-09-26 01:14:42 +0300

    jd01_01 arhipova complete

---
+++ src/by/it/arhipova/jd01_05/TaskA.java
@@ -0,0 +1,13 @@
+package by.it.arhipova.jd01_05;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static java.lang.StrictMath.*;
+
+public class TaskA {
+    public static void main(String[] args) {
+        step1();
+    }
+    
+    }
+}
