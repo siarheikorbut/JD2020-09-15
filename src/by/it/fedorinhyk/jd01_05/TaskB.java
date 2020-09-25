5d2f352ae97901c6122bcb049365a25c74cf8901
4rank <frankensteinn.v@gmail.com>
2020-09-25 14:58:11 +0300

    TaskB2 ready

--- src/by/it/fedorinhyk/jd01_05/TaskB.java
+++ src/by/it/fedorinhyk/jd01_05/TaskB.java
@@ -20,28 +20,30 @@ public class TaskB {
     }
 
 
-
     private static void step5() {
-        double a;
-        double b = 0;
-        for(double x=-5.5; x<2; x=x+0.5){
-            if ( x/2<=-1 || x/2>-2) b = sin(pow(x, 2));
-                a=log(abs(b+2.74));
-
-
-            if (-1<x/2 || x/2<0.2) b = cos(pow(x, 2));
-                a=log(abs(b+2.74));
-
-            if (x / 2 == 0.2) b=1/tan(pow(x,2));
-                a=log(abs(b+2.74));
-
-            System.out.printf("При x/2=%3.2f a = %g\n",x/2,a);
-
+        double b;
+        for (double x = -5.5; x < 2; x = x + 0.5) {
+            if (x / 2 <= -1 && x / 2 > -2) {
+                b = sin(pow(x, 2));
+                double a = log10(abs(b + 2.74));
+                System.out.printf("При x/2=%3.2f a = %g\n", x / 2, a);
+            }
+             else if (-1 < x / 2 && x / 2 < 0.2) {
+                b = cos(pow(x, 2));
+                double a = log10(abs(b + 2.74));
+                System.out.printf("При x/2=%3.2f a = %g\n", x / 2, a);
+            }
+            else if (x / 2 == 0.2) {
+                b = 1 / tan(pow(x, 2));
+                double a = log10(abs(b + 2.74));
+                System.out.printf("При x/2=%3.2f a = %g\n", x / 2, a);
+            }
+            else {
+                System.out.printf("При x/2=%3.2f вычисления не определены\n", x / 2);
+            }
 
         }
 
-
     }
-
 
 }
