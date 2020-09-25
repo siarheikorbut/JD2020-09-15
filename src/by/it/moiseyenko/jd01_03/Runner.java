ecb970f01b77d0a67aedd88bc30f1345e9db3011
MoiseyenkaArtsiom <5700753@gmail.com>
2020-09-26 00:23:18 +0300

    JD01_01 - JD01_04

---
+++ src/by/it/moiseyenko/jd01_03/Runner.java
@@ -0,0 +1,14 @@
+package by.it.moiseyenko.jd01_03;
+
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str= sc.nextLine();
+        double[] array = InOut.getArray(str);
+        InOut.printArray(array);
+        InOut.printArray(array,"V",3);
+    }
+}
+
---
+++ src/by/it/moiseyenko/jd01_04/TaskB.java
@@ -0,0 +1,18 @@
+package by.it.moiseyenko.jd01_04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class TaskB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+    }
+    InputStreamReader name=new InputStreamReader(System.in);
+    BufferedReader line=new BufferedReader(name);
+    String out= line.readLine();
+
+
+
+}
