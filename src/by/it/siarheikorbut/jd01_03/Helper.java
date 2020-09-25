7b7b78536c140531ec9d828aa79149972a4da221
Сергей Корбут <siarheikorbut@gmail.com>
2020-09-26 00:08:13 +0300

    jd01_03 TaskB,C completed.

--- src/by/it/siarheikorbut/jd01_03/Helper.java
+++ src/by/it/siarheikorbut/jd01_03/Helper.java
@@ -2,38 +2,47 @@ package by.it.siarheikorbut.jd01_03;
 
 import java.util.Arrays;
 
+/**
+ * @author Siarhei Korbut
+ * @see <a href="https://drive.google.com/file/d/1L5Z3U4Iv8EyTak0vgQ3IvNlZMXvPONAH/view?usp=sharing">Задания JD01_03 ( B, C )</a>
+ */
 
 public class Helper {
+
+    //Поиск минимального значения в одномерном массиве типа double.
     static double findMin(double[] arr) {
         double minValue = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < minValue) {
-                minValue = arr[i];
+        for (double v : arr) {
+            if (v < minValue) {
+                minValue = v;
             }
         }
         return minValue;
     }
 
+    //Поиск максимального значения в одномерном массиве типа double.
     static double findMax(double[] arr) {
         double maxValue = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > maxValue) {
-                maxValue = arr[i];
+        for (double v : arr) {
+            if (v > maxValue) {
+                maxValue = v;
             }
         }
         return maxValue;
     }
 
+    //Сортировка по возрастанию одномерных массивов типа double.
     static void sort(double[] arr) {
         double arrSort = arr[0];
         Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < arrSort) {
-                arrSort = arr[i];
+        for (double v : arr) {
+            if (v < arrSort) {
+                arrSort = v;
             }
         }
     }
 
+    //Умножение матрицы на вектор типа double.
     static double[] mul(double[][] matrix, double[] vector) {
         double[] resVector = new double[matrix.length];
         for (int i = 0; i < resVector.length; i++) {
@@ -46,6 +55,7 @@ public class Helper {
         return resVector;
     }
 
+    //Умножение матрицы на матрицу типа double.
     static double[][] mul(double[][] matrixLeft, double[][] matrixRight) {
         double m = matrixLeft.length;
         double n = matrixRight[0].length;
@@ -55,7 +65,7 @@ public class Helper {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < o; k++) {
-                    resMatrix[i][j]+=matrixLeft[i][k]*matrixRight[k][j];
+                    resMatrix[i][j] += matrixLeft[i][k] * matrixRight[k][j];
                 }
             }
         }
