3906e3e7212bd6a70f141369fa058c3f28466b41
Сергей Корбут <siarheikorbut@gmail.com>
2020-09-26 12:42:29 +0300

    jd01_06 Task A1,2 completed

---
+++ src/by/it/siarheikorbut/jd01_05/TaskB.java
@@ -0,0 +1,10 @@
+package by.it.siarheikorbut.jd01_05;
+
+/**
+ * @author Siarhei Korbut
+ * @see <a href="https://drive.google.com/file/d/1KNBofHHOxyARzccIFNqRvxU8ks8NY0n4/view?usp=sharing">Задание JD01_05 ( B )</a>
+ */
+
+public class TaskB {
+
+}
---
+++ src/by/it/siarheikorbut/jd01_06/TaskB1.java
@@ -0,0 +1,12 @@
+package by.it.siarheikorbut.jd01_06;
+
+/**
+ * @author Siarhei Korbut
+ * @see <a href="https://drive.google.com/file/d/184w9bSZcb6EcCcpfp_ff1nl2atlGKnpz/view?usp=sharing">Задание JD01_06 ( B1 )</a>
+ */
+
+public class TaskB1 {
+    public static void main(String[] args) {
+
+    }
+}
