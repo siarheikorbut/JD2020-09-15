752149ea4a8578beeb0c631102d60ccfd95d2a60
DevForEnjoy <49254510+DevForEnjoy@users.noreply.github.com>
2020-09-26 11:14:32 +0300

    Edit Jd01_05 TaskC

--- src/by/it/lapkovskiy/jd01_05/TaskC.java
+++ src/by/it/lapkovskiy/jd01_05/TaskC.java
@@ -7,8 +7,6 @@ public class TaskC {
     public static void main(String[] args) {
         Random random = new Random();
          step1(random.nextInt(20)+20);
-        for (int i = 8000; i < 8500; i++) {
-            pr(Character.toString(i),2);}
     }
     static void  step1(int a){
         Random random = new Random();
@@ -60,58 +58,5 @@ public class TaskC {
         }
         System.out.printf("\n %.5f",Math.pow(mulArr,1.0/unitM));
     }
-    static void  step2(int a){
-        Random random = new Random();
-        double[] arrayA = new double[a];
-        int row = 0;
-        double x;
-        double z;
-        int unitM =0;
-        System.out.println("Массив A[]");
 
-
-        for (int i = 0; i < arrayA.length; i++) {
-            x = (random.nextInt(367) + 533) / 100.0;
-            z = Math.pow(x * x + 4.5, 1 / 3.0);
-            if(z>3.5){
-                unitM++;
-            }
-            arrayA[i] = z;
-            System.out.printf("A[ %-2d] = %-10.5f ", i, z);
-            if (row == 4) {
-                System.out.println();
-                row = 0;
-                continue;
-            }
-            row++;
-        }
-        System.out.println();
-        double[] arrayB = new double[unitM];
-        int arr=0;
-        double mulArr = 1;
-        for (int i = 0; i < arrayA.length; i++) {
-            if(arrayA[i]>3.5){
-                arrayB[arr] = arrayA[i];
-                mulArr= mulArr * arrayB[arr];
-                arr++;
-            }
-        }
-        row = 0;
-        System.out.println("Массив B[] из элементов массива A > 3.5");
-        for (int i = 0; i < arrayB.length; i++) {
-            System.out.printf("B[ %-2d] = %-10.5f ", i, arrayB[i]);
-            if (row == 4) {
-                System.out.println();
-                row = 0;
-                continue;
-            }
-            row++;
-        }
-        System.out.printf("\n %.5f",Math.pow(mulArr,1.0/unitM));
-    }
-    static void pr(String a, int col){
-        for (int i = 0; i < col; i++) {
-            System.out.print(a);
-        }
-    }
 }
