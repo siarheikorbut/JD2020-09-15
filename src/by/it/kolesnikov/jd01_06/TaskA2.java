828484c7a0f2a05c5d5fdba912f5ad83ac05b055
Ghostik13 <roxan95@,ail.ru>
2020-09-26 12:27:44 +0300

    jd01_06 TaskA1 and B1 complete

---
+++ src/by/it/kolesnikov/jd01_06/TaskA2.java
@@ -0,0 +1,30 @@
+package by.it.kolesnikov.jd01_06;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskA2 {
+    private static String[] uniqueWords={};
+    private static int[] counters={};
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder(Poem.text);
+        Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]+");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            String word = matcher.group();
+            processOneWord(word);
+        }
+        for (int i = 0; i < uniqueWords.length; i++) {
+            System.out.printf("%s=%d\n",uniqueWords[i], counters[i]);
+        }
+    }
+    private static void processOneWord(String word){
+        for (int i = 0; i < uniqueWords.length; i++) {
+            if(uniqueWords[i].equals(uniqueWords)){
+                counters[i]++;
+                return;
+            }
+        }
+    }
+}
---
+++ src/by/it/kolesnikov/jd01_06/TaskB1.java
@@ -0,0 +1,30 @@
+package by.it.kolesnikov.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+    private static String[] uniqueWords = {};
+    private static int[] counters = {};
+
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder(Poem.text);
+        Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]+");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            String word = matcher.group();
+            if (checkWord(word)) {
+                System.out.println(word);
+            }
+        }
+    }
+            private static final String votes="уеэоаыяиюёУЕЭОАЫЯИЮЁ";
+
+            private static boolean checkWord (String word){
+                char first = word.charAt(0);
+                char last = word.charAt(word.length() - 1);
+                boolean firstOk=votes.indexOf(first)<0;
+                boolean lastOk=votes.indexOf(last)>=0;
+                return firstOk && lastOk;
+            }
+        }
