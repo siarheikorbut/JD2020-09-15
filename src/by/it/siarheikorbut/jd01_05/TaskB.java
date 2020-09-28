ea7fc9c794852007fe66084de69569f529991496
Сергей Корбут <siarheikorbut@gmail.com>
2020-09-28 19:49:46 +0300

    jd01_05 Task B1 completed

--- src/by/it/siarheikorbut/jd01_05/TaskB.java
+++ src/by/it/siarheikorbut/jd01_05/TaskB.java
@@ -6,5 +6,23 @@ package by.it.siarheikorbut.jd01_05;
  */
 
 public class TaskB {
+    public static void main(String[] args) {
+        stet1();
+    }
 
+    private static void stet1() {
+
+        //Вычисление суммы
+        double y;
+        double result = 0;
+        for (double i = 0; i <= 2; i = i + 0.2) {
+            for (int j = 1; j <= 6; j++) {
+                y = (Math.pow(7, i)) - (Math.cos(j));
+                result += y;
+            }
+            System.out.printf("При а = %.2f Сумма y = %g%n", i, result);
+            result = 0;
+        }
+        System.out.println();
+    }
 }
