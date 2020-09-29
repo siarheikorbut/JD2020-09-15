9c9b0446f4f2030f399876c70045fcb9760784bc
MoiseyenkaArtsiom <5700753@gmail.com>
2020-09-29 06:15:46 +0300

    jd01 06 TaskA1 A2

--- src/by/it/moiseyenko/jd01_04/TaskB.java
+++ src/by/it/moiseyenko/jd01_04/TaskB.java
@@ -7,36 +7,32 @@ import java.util.Scanner;
 public class TaskB {
     public static void main(String[] args) {
 
-        //Ввод с консоли колличества работников.
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        //Ввод одномерного массива фамилий работников
         String[] lastname = new String[n];
         for (int i = 0; i < n; i++) {
             lastname[i] = sc.next();
         }
-        //Обьявление и инициализация переменных.
         int sumSalary = 0;
         int totalSalary = 0;
         double averSalary = 0;
 
-        //Ввод двумерного массива для зарплат работников за четыре квартала.
         int[][] salary = new int[n][4];
         for (int i = 0; i < salary.length; i++) {
             System.out.println("Введите зарплату для " + lastname[i]);
             for (int j = 0; j < salary[i].length; j++) {
                 salary[i][j] = sc.nextInt();
 
-                //Подсчет общей суммы всех выплат
+
                 totalSalary += salary[i][j];
 
-                //Подсчет среднеарифметической квартальной зарплаты с точностью до четвертого знака.
+
                 averSalary = totalSalary / (n * 4.0);
             }
         }
 
-        //Построение таблицы доходов работников с фамилиями по кварталам и итогом за год.
         System.out.println("---------------------------------------------------------------");
         System.out.printf("%-10s%-12s%-12s%-12s%-12s%-12s\n", "Фамилия", "Квартал 1", "Квартал 2", "Квартал 3", "Квартал 4", "Итого");
         System.out.println("---------------------------------------------------------------");
@@ -46,10 +42,9 @@ public class TaskB {
         }
         System.out.println("---------------------------------------------------------------");
 
-        //Вывод общей суммы всех выплат.
+
         System.out.printf("%-10s%-12d\n", "Итого", sumSalary);
 
-        //Вывод среднеарифметической квартальной зарплаты.
         System.out.printf("%-10s%-12.6f\n", "Средняя", averSalary);
     }
 
