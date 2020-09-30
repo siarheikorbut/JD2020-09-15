74273d522cdaeb71cd229502b58321ecfd99193c
MoiseyenkaArtsiom <5700753@gmail.com>
2020-09-30 10:09:04 +0300

    jd01 07 done for B2

--- src/by/it/moiseyenko/jd01_07/Var.java
+++ src/by/it/moiseyenko/jd01_07/Var.java
@@ -1,5 +1,8 @@
 package by.it.moiseyenko.jd01_07;
 
-public abstract class Var {
-
+class Var {
+    @Override
+    public String toString() {
+        return "Это класс Var";
+    }
 }
---
+++ src/by/it/moiseyenko/jd01_07/Vector.java
@@ -0,0 +1,29 @@
+package by.it.moiseyenko.jd01_07;
+
+import java.util.Arrays;
+
+class Vector extends Var {
+    private double[] value;
+    Vector(double[] value){
+        this.value=value;
+    }
+
+    Vector(Vector vector) {
+        this.value=vector.value;
+    }
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb=new StringBuilder("{");
+        String delimiter="";
+        for (double element:value){
+            sb.append(delimiter).append(element);
+            delimiter=", ";
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+}
+
