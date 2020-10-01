e839b97c2cb51928c8cd9f188b02ae01be590c52
Maks7117 <m6104140@yandex.ru>
2020-10-01 18:08:11 +0200

    jd01_07

--- src/by/it/sheremet/jd01_06/TaskB2.java
+++ src/by/it/sheremet/jd01_06/TaskB2.java
@@ -1,62 +1,25 @@
 package by.it.sheremet.jd01_06;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB2 {
    public static void main(String[] args) {
        StringBuilder text = new StringBuilder(Poem.text);
-       Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]+");
+       Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]+[\\.|.\\{3}|,|!]+");
        Matcher matcher = pattern.matcher(Poem.text);
        while (matcher.find()) {
            String word = matcher.group();
-           word=word.replace('о','J' );
-           //System.out.println(word);
+           word = word.replaceAll("[^а-яёА-ЯЁ]+", " ");
+          /* word = word.trim();
+          /* wordText
+           //System.out.print(words[i]+"\n");
 
-      }
-      // System.out.println(text);
 
-       //System.out.println(text);
-       /*Pattern pattern = Pattern.compile("-:!");
-       Matcher matcher= pattern.matcher(text);
-       while (matcher.find()){
-           String word = matcher.group();
-           int pos = matcher.start();
-           if (checkWord(word)){
-               System.out.println(word);
-           }
+       private static wordText(String word){
+
        }*/
-       //String str="Asdsfd df dsv. Ssd gfn fdf fdfddf. Jsdf ler sdc";
-
-       String[] words = Poem.text.split("\\.|\\.{3}|,|!|-|:");
-       String[] trimledArray = new String[words.length];
-       int j=0;
-       for (int i = 0; i < words.length-1; i++) {
-          // for (int j = 0; j < words.length; j++) {
-                String str=words[i].trim().replaceAll(" +"," ");
-                if(!(str.equals(""))){
-                    trimledArray[i] = str;
-                    j++;
-                    System.out.println(str);{
-
-                    }
-
-              if(trimledArray[i].length()>trimledArray[i+1].length()) {
-                   String sort = trimledArray[i];
-                   trimledArray[i] = trimledArray[i + 1];
-                   trimledArray[i + 1] = sort;
-               }
-
-           }
-       }
-
-
-       for (int z = 0; z < trimledArray.length; z++) {
-           System.out.println(trimledArray[z]+"\n");
-       }
-       // text.replace("!" " ")
-       //System.out.println(text);*/
    }
-
 }
-
+}
