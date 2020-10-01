0786059bd802c02b8b719c04f6a29e32ddcc4fdc
Eugen Sretensky <djerikus@gmail.com>
2020-10-01 12:29:01 +0300

    jd07_01

---
+++ src/by/it/sheremet/jd01_08/Matrix.java
@@ -0,0 +1,4 @@
+package by.it.sheremet.jd01_08;
+
+public class Matrix {
+}
---
+++ src/by/it/sheremet/jd01_08/Var.java
@@ -0,0 +1,9 @@
+package by.it.sheremet.jd01_08;
+
+abstract class Var {
+
+    @Override
+    public String toString() {
+        return "abstract Var";
+    }
+}
