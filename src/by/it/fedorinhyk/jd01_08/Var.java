bd3cf6cc4d68e7890c181f5fae44c547e5af8c75
Nikolai-Barabash <nickbarabash83@gmail.com>
2020-10-01 12:28:54 +0300

    jd01_08 classwork

---
+++ src/by/it/fedorinhyk/jd01_08/Var.java
@@ -0,0 +1,23 @@
+package by.it.fedorinhyk.jd01_08;
+
+abstract class Var implements Operation {
+    @Override
+    public Var add(Var other) {
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        return null;
+    }
+}
---
+++ src/by/it/fedorinhyk/jd01_08/Vector.java
@@ -0,0 +1,36 @@
+package by.it.fedorinhyk.jd01_08;
+
+import java.util.Arrays;
+
+class Vector extends Var {
+    Object strVector;
+    private double[] value;
+
+    Vector (double[]value){
+        this.value=value;
+    }
+
+    Vector (Vector vector){
+        double[] tmp = vector.value;
+        this.value = Arrays.copyOf(tmp,tmp.length);
+    }
+    Vector (String strVector) {
+        double[] str = new double[value.length];
+        for (int i = 0; i < value.length; i++) {
+            str[i] = Double.parseDouble(strVector);
+        }
+        this.value[Integer.parseInt(strVector)] = Double.parseDouble(strVector);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sbv=new StringBuilder("{");
+        String delimiter="";
+        for (double element : value){
+            sbv.append(delimiter).append(element);
+            delimiter=", ";
+        }
+        sbv.append("}");
+        return sbv.toString();
+    }
+}
