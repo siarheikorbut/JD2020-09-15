f5283cfb508ba3b2c9c9748072ab1f21438df115
Ghostik13 <roxan95@,ail.ru>
2020-10-01 11:41:21 +0300

    jd01_06 TaskB2 complete

---
+++ src/by/it/kolesnikov/jd01_07/Vector.java
@@ -0,0 +1,34 @@
+package by.it.kolesnikov.jd01_07;
+
+import java.util.Arrays;
+
+class Vector extends Var {
+
+    private double[] value;
+
+    public Vector(double[] value) {
+
+        this.value = Arrays.copyOf(value,value.length);
+    }
+
+    public Vector(Vector other) {
+        double[] tmp = other.value;
+        this.value = Arrays.copyOf(tmp,tmp.length);
+    }
+
+    public Vector(String strValue){
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder out = new StringBuilder();
+        out.append('{');
+        String delimiter="";
+        for (double element : value) {
+            out.append(delimiter).append(element);
+            delimiter=", ";
+        }
+        out.append('}');
+        return out.toString();
+    }
+}
