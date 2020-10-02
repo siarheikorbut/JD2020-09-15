93c4bc268ecb2d15f51b9ad4f555f15efb48988d
4rank <frankensteinn.v@gmail.com>
2020-10-02 20:11:12 +0300

    jd01_08 B3 ready.

--- src/by/it/fedorinhyk/jd01_07/Matrix.java
+++ src/by/it/fedorinhyk/jd01_07/Matrix.java
@@ -15,21 +15,40 @@ class Matrix extends Var {
     }
 
     Matrix (String strMatrix){
+        String mat = strMatrix;
+        mat = mat.replaceFirst("\\{\\{","").replaceFirst("}}","");
+        String[] matvec = mat.split("},\\{");
+        String[][] matstr = new String[matvec.length][];
+        for (int i = 0; i < matvec.length; i++) {
+            matstr[i] = matvec[i].split(",");
+        }
+        double[][] fin = new double[matvec.length][matstr[0].length];
+        for (int i = 0; i < matstr.length; i++) {
+            for (int j = 0; j < matstr[0].length; j++) {
+                fin[i][j] = Double.parseDouble(matstr[i][j]);
+            }
+        }
+        this.value = fin;
     }
 
     @Override
     public String toString() {
-        StringBuilder sbv=new StringBuilder("{");
-        String delimiter="";
-        for (double[] element : value){
-            sbv.append(delimiter).append(element);
-            delimiter=", ";
-            for (double elements : element) {
-                sbv.append("{").append(delimiter).append(elements).append("}");
-                delimiter=", ";
+        StringBuilder sbv = new StringBuilder();
+        sbv.append("{");
+        for (int i = 0; i < value.length ; i++) {
+            sbv.append("{");
+            String delimiter="";
+            for (int j = 0; j < value[0].length; j++) {
+                sbv.append(delimiter).append(value[i][j]);
+                delimiter=",";
+            }
+            sbv.append("}");
+            if(i< (value.length-1)){
+                sbv.append(delimiter);
             }
         }
         sbv.append("}");
         return sbv.toString();
     }
+
 }
