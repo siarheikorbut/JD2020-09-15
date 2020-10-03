2f40724ad69d9c4430b14d850ffa4629ca6bcccb
mariarhipova <maria.arhipova92@gmail.com>
2020-10-03 13:17:32 +0300

    jd01_09 arhipova

---
+++ src/by/it/arhipova/jd01_09/Patterns.java
@@ -0,0 +1,9 @@
+package by.it.arhipova.jd01_09;
+
+public interface Patterns {
+    String OPERATION ="[-+/*]";
+    String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+    String VECTOR = "\\{((=?[0-9]+\\.?[0-9]*),?)+}";
+    String MATRIX = "\\{(\\{((-?[0-9]+\\.?[0-9]*),?)+},?)+}";
+
+}
