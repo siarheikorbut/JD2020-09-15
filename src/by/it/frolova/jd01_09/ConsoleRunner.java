9c7fb64b157140d4994fe5284e3aa6327c89d54c
jmabracodabra <jmabracodabra@gmail.com>
2020-10-04 22:25:00 +0300

    implemented jd01_09

--- src/by/it/frolova/jd01_09/ConsoleRunner.java
+++ src/by/it/frolova/jd01_09/ConsoleRunner.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class ConsoleRunner {
     public static void main(String[] args) {
 
-
         Scanner sc = new Scanner(System.in);
         Parser parser = new Parser();
         Printer printer = new Printer();
 
-        for (;;) {
+        for (; ; ) {
             String expression = sc.nextLine();
             if (expression.equals("end")) {
                 break;
             } else {
-
+                Var result = parser.calc(expression);
+                printer.print(result);
             }
         }
     }
--- src/by/it/frolova/jd01_09/Var.java
+++ src/by/it/frolova/jd01_09/Var.java
@@ -1,6 +1,19 @@
 package by.it.frolova.jd01_09;
 
 public abstract class Var implements Operation {
+
+    static Var createVar(String strVar) {
+        if (strVar.matches(Patterns.SCALAR)) {
+            return new Scalar(strVar);
+        } else if (strVar.matches(Patterns.VECTOR)) {
+            return new Vector(strVar);
+        } else if (strVar.matches(Patterns.MATRIX)) {
+            return new Matrix(strVar);
+        } else
+            System.out.println("Unknown variable: " + strVar);
+        return null;
+    }
+
     @Override
     public Var add(Var other) {
         //TODO create exception
