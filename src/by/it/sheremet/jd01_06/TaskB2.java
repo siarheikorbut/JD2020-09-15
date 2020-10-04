0dd3efd3ce454619409babbc86d194f8e28b84eb
Maks7117 <m6104140@yandex.ru>
2020-10-04 21:34:41 +0200

    jd01_08

--- src/by/it/sheremet/jd01_06/TaskB2.java
+++ src/by/it/sheremet/jd01_06/TaskB2.java
@@ -6,20 +6,31 @@ import java.util.regex.Pattern;
 
 public class TaskB2 {
    public static void main(String[] args) {
-       StringBuilder text = new StringBuilder(Poem.text);
-       Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]+[\\.|.\\{3}|,|!]+");
-       Matcher matcher = pattern.matcher(Poem.text);
-       while (matcher.find()) {
-           String word = matcher.group();
-           word = word.replaceAll("[^а-яёА-ЯЁ]+", " ");
-          /* word = word.trim();
-          /* wordText
-           //System.out.print(words[i]+"\n");
+       String text=Poem.text;
+       text=text.replaceAll("\\.\\.\\.","");
+       text=text.replaceAll("","");
+       System.out.println(text);
+       String [] sent=text.split("[.!?]");
+       System.out.println(sent);
+       for (int i = 0; i < sent.length; i++) {
+           sent[i]=sent[i].replaceAll("[^А-ЯЁа-яё]+"," ");
+           sent[i]=sent[i].trim();
+           //System.out.println(sent[i]);
+       }
+       for (int i = sent.length-1; i > 0; i--) {
+           for (int j = 0; j <sent.length-1 ; j++) {
+                String save;
+           if (sent[j].length()>sent[j+1].length()) {
 
+               save=sent[j];
+               sent[j]=sent[j+1];
+               sent[j+1]=save;
+           }
+           }
+       }
+       for (int i = 0; i < sent.length; i++) {
+          System.out.print(sent[i]+"\n");
+       }
 
-       private static wordText(String word){
-
-       }*/
    }
-}
 }
