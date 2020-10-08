3796c2687d3a12211376b3d90c3824acbdf38895
Сергей Корбут <siarheikorbut@gmail.com>
2020-10-08 12:05:50 +0300

    jd01_11 Task A completed

---
+++ src/by/it/siarheikorbut/jd01_11/Runner.java
@@ -0,0 +1,28 @@
+package by.it.siarheikorbut.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+
+        list = new ListA<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+    }
+}
