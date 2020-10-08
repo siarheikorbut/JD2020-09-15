a36191470d53c9c0d14c7f4f36b58bf1c2b1ca93
Nikolai-Barabash <nickbarabash83@gmail.com>
2020-10-08 12:26:44 +0300

    jd01_11 classwork

---
+++ src/by/it/fedorinhyk/jd01_11/Runner.java
@@ -0,0 +1,13 @@
+package by.it.fedorinhyk.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> List = new ArrayList<>();
+    }
+
+
+
+}
