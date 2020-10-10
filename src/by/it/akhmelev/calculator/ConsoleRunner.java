f3ac55e9e77c48ce9c76a52467613cbcad6491f7
Olga <https://github.com/OlgaDobrodey/JD2020-09-15>
2020-10-10 22:07:34 +0300

    Calc (level B,C) Finish

--- src/by/it/akhmelev/calculator/ConsoleRunner.java
+++ src/by/it/akhmelev/calculator/ConsoleRunner.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 
 public class ConsoleRunner {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         Parser parser = new Parser();
         Printer printer = new Printer();
-        for(;;){
+        for (; ; ) {
             String expression = sc.nextLine();
             if (expression.equals("end")) break;
             Var result = parser.calc(expression);
             printer.print(result);
         }
-
     }
 }
