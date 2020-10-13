347ca05d4446ea575b4cf8ddb3f07e979671ca4b
YauhenDubrovin <yauhen_dubrovin@yahoo.co.uk>
2020-10-13 12:16:03 +0300

    jd01_13 TaskA 1 completed

---
+++ src/by/it/siarheikorbut/jd01_13/TaskA.java
@@ -0,0 +1,26 @@
+package by.it.siarheikorbut.jd01_13;
+
+import java.util.HashMap;
+
+public class TaskA {
+    public static void main(String[] args) {
+        try {
+            if (Math.random() > 0.5)
+                new HashMap<String, String>(null);
+            else Integer.parseInt("Привет");
+        } catch (NumberFormatException | NullPointerException e) {
+            Class<? extends RuntimeException> aClass = e.getClass();
+            String exceptionName = aClass.getName();
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            for (StackTraceElement traceElement : stackTrace) {
+                String methodName = traceElement.getMethodName();
+                if (methodName.equals("main")) {
+                    String className = traceElement.getClassName();
+                    int lineNumber = traceElement.getLineNumber();
+                    System.out.printf("  name: %s\n class: %s\n line:d\n", exceptionName, className, lineNumber);
+                    break;
+                }
+            }
+        }
+    }
+}
