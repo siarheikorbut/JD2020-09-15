3d9866ec835fd3f4da70816af508157490798fdc
igor <gob1in@Mail.ru>
2020-10-13 09:47:20 +0300

    jd01_09

--- src/by/it/dobrodey/jd01_12/TaskB3.java
+++ src/by/it/dobrodey/jd01_12/TaskB3.java
@@ -24,11 +24,11 @@ public class TaskB3 {
         System.out.println(peopleList2);
         Long t = System.nanoTime();
         String name1 = process(peopleList1);
-        Long tA = System.nanoTime() - t;
+        Long tA = System.nanoTime()-t;
         System.out.println("name1 = " + name1);
         Long t1 = System.nanoTime();
         String name2 = process(peopleList2);
-        Long tL = System.nanoTime() - t1;
+        Long tL = System.nanoTime() - 2*t1;
         System.out.println("name2 = " + name2);
         System.out.println(" Время работы для  ArrayList=" + tA / 1000 + " мкс.");
         System.out.println(" Время работы для  LinkedList=" + tL / 1000 + " мкс.");
