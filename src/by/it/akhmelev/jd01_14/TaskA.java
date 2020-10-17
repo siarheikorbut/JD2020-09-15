9c9dfc70b733d21887a9987dbedf2928b113de1f
IT-Academy <git@itacademy.by>
2020-10-17 12:29:55 +0300

    classwork jd01_15

--- src/by/it/akhmelev/jd01_14/TaskA.java
+++ src/by/it/akhmelev/jd01_14/TaskA.java
@@ -12,17 +12,6 @@ class TaskA {
     private static final String USER_DIR = "user.dir";
     private static final String RESULT_TASK_A = "resultTaskA.txt";
 
-    @SuppressWarnings("SameParameterValue")
-    private static String getPath(Class<?> aClass) {
-        String packageName = aClass
-                .getPackage()
-                .getName()
-                .replace(".", File.separator)
-                .concat(File.separator);
-        String root = System.getProperty(USER_DIR);
-        return root + File.separator + SRC + File.separator + packageName;
-    }
-
 
     public static void main(String[] args) {
         String filename = getPath(TaskA.class) + FILE_DATA;
@@ -32,6 +21,18 @@ class TaskA {
         printToConsole(list);
         String filenameTxt = getPath(TaskA.class) + RESULT_TASK_A;
         printToFile(list, filenameTxt);
+    }
+
+
+    @SuppressWarnings("SameParameterValue")
+    private static String getPath(Class<?> aClass) {
+        String packageName = aClass
+                .getPackage()
+                .getName()
+                .replace(".", File.separator)
+                .concat(File.separator);
+        String root = System.getProperty(USER_DIR);
+        return root + File.separator + SRC + File.separator + packageName;
     }
 
     private static void writeRandomInt(String filename) {
