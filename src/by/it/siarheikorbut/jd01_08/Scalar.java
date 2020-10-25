7c6c6b8d41230b9de77f18b11829d335b3207d46
siarheikorbut <siarheikorbut@gmail.com>
2020-10-25 22:14:29 +0300

    jd01_08 Task A, B, C completed

--- src/by/it/siarheikorbut/jd01_08/Scalar.java
+++ src/by/it/siarheikorbut/jd01_08/Scalar.java
@@ -7,68 +7,68 @@ package by.it.siarheikorbut.jd01_08;
 
 //Создание дочернего от класса Var класса Scalar.
 public class Scalar extends Var {
+    private final double value;
 
-    //Обьявление приватной финализированной переменной value.
-    private final double VALUE;
-
-    public double getVALUE() {
-        return VALUE;
+    public double getValue() {
+        return value;
     }
 
-    //Создание конструктора принимающего в себя переменную value.
-    Scalar(double value) {
-        this.VALUE = value;
-    }
-
-    //Создание конструктора принимающего в себя экземпляр класса Scalar.
-    Scalar(Scalar scalar) {
-        this.VALUE = scalar.VALUE;
-    }
-
-    //Создание конструктора принимающего в себя строку и преобразующего её в число.
-    Scalar(String strScalar) {
-        this.VALUE = Double.parseDouble(strScalar);
-    }
-
-    //Переопределение метода toString.
     @Override
     public String toString() {
-        return Double.toString(VALUE);
+        return Double.toString(value);
     }
 
-    //Метод сложения скаляров.
+    Scalar(double value) {
+        this.value = value;
+    }
+
+    Scalar(String strScalar) {
+        this.value = Double.parseDouble(strScalar);
+    }
+
+    Scalar(Scalar scalar) {
+        this.value = scalar.value;
+    }
+
     @Override
     public Var add(Var other) {
         if (other instanceof Scalar) {
-            double sum = this.VALUE + ((Scalar) other).VALUE;
-            return new Scalar(sum);
-        } else return other.add(this);
+            double result = this.value + ((Scalar) other).value;
+            return new Scalar(result);
+        } else {
+            return other.add(this);
+        }
     }
 
-    //Метод вычитания скаляров.
     @Override
     public Var sub(Var other) {
         if (other instanceof Scalar) {
-            double sub = this.VALUE - ((Scalar) other).VALUE;
-            return new Scalar(sub);
-        } else return new Scalar(-1).mul(other).add(this);
+            double result = this.value - ((Scalar) other).value;
+            return new Scalar(result);
+        } else {
+            return new Scalar(-1).mul(this).add(other);
+        }
     }
 
-    //Метод умножения скаляров
     @Override
     public Var mul(Var other) {
         if (other instanceof Scalar) {
-            double mul = this.VALUE * ((Scalar) other).VALUE;
-            return new Scalar(mul);
-        } else return other.mul(this);
+            double result = this.value * ((Scalar) other).value;
+            return new Scalar(result);
+        } else {
+            return other.mul(this);
+        }
     }
 
-    //Метод деления скаляров.
     @Override
     public Var div(Var other) {
         if (other instanceof Scalar) {
-            double div = this.VALUE / ((Scalar) other).VALUE;
-            return new Scalar(div);
-        } else return super.div(other);
+            double result = this.value / ((Scalar) other).value;
+            return new Scalar(result);
+        } else if (other instanceof Vector) {
+            return other.div(this);
+        } else {
+            return super.div(other);
+        }
     }
 }
--- src/by/it/siarheikorbut/jd01_08/Vector.java
+++ src/by/it/siarheikorbut/jd01_08/Vector.java
@@ -1,6 +1,8 @@
 package by.it.siarheikorbut.jd01_08;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Siarhei Korbut
@@ -10,113 +12,114 @@ import java.util.Arrays;
 //Создание дочернего от класса Var класса Vector
 public class Vector extends Var {
 
-    //Обьявление приватного финализированного одномерного массива value.
-    private final double[] VALUE;
+    private final double[] value;
 
-    //Создание конструктора принимающего в себя массив value.
-    Vector(double[] value) {
-        this.VALUE = value;
+    public double[] getValue() {
+        return value;
     }
 
-    //Создание конструктора принимающего в себя экземпляр класса Vector.
-    Vector(Vector vector) {
-        this.VALUE = vector.VALUE;
-    }
-
-    //Создание конструктора принимающего в себя строку и преобразующего её в массив чисел.
     Vector(String strVector) {
-        String[] s1 = strVector.replaceAll("\\{", "").replaceAll("}", "").split(",");
-        this.VALUE = new double[s1.length];
-        for (int i = 0; i < s1.length; i++) {
-            VALUE[i] = Double.parseDouble(s1[i].trim());
+        Pattern pattern = Pattern.compile("[{}]");
+        Matcher matcher = pattern.matcher(strVector);
+        while (matcher.find()) {
+            strVector = matcher.replaceAll(" ");
         }
+        double[] line = new double[strVector.trim().split(",\\s*").length];
+        for (int i = 0; i < line.length; i++) {
+            line[i] = Double.parseDouble(strVector.trim().split(",\\s*")[i]);
+        }
+        this.value = line;
     }
 
-    //Переопределение метода toString для вывода одномерного массива.
+    Vector(Vector vector) {
+        this.value = Arrays.copyOf(vector.value, vector.value.length);
+    }
+
+    Vector(double[] value) {
+        this.value = Arrays.copyOf(value, value.length);
+    }
+
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("{");
-        String delimiter = "";
-        for (double element : VALUE) {
-            sb.append(delimiter).append(element);
-            delimiter = ", ";
+        StringBuilder line = new StringBuilder("{");
+        for (int i = 0; i < value.length; i++) {
+            if (i == value.length - 1) {
+                line.append(value[i]).append("}");
+                continue;
+            }
+            line.append(value[i]).append(", ");
         }
-        sb.append("}");
-        return sb.toString();
+        return line.toString();
     }
 
-    //Метод сложения скаляра и вектора, вектора и вектора.
     @Override
     public Var add(Var other) {
-        //Сложение скаляра и вектора.
         if (other instanceof Scalar) {
-            double[] res = Arrays.copyOf(VALUE, VALUE.length);
-            for (int i = 0; i < res.length; i++) {
-                res[i] = res[i] + ((Scalar) other).getVALUE();
+            double[] result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] += ((Scalar) other).getValue();
             }
-            return new Vector(res);
-            //Сложение вектора и вектора.
+            return new Vector(result);
         } else if (other instanceof Vector) {
-            double[] res = Arrays.copyOf(VALUE, VALUE.length);
-            for (int i = 0; i < res.length; i++) {
-                res[i] = res[i] + ((Vector) other).VALUE[i];
+            double[] result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] += ((Vector) other).value[i];
             }
-            return new Vector(res);
-        } else
-            //Сложение вектора и матрицы является ошибкой.
+            return new Vector(result);
+        } else {
             return super.add(other);
+        }
     }
 
     @Override
     public Var sub(Var other) {
         if (other instanceof Scalar) {
-            double[] sub = Arrays.copyOf(VALUE, VALUE.length);
-            for (int i = 0; i < sub.length; i++) {
-                sub[i] -= ((Scalar) other).getVALUE();
+            double[] result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] -= ((Scalar) other).getValue();
             }
-            return new Vector(sub);
+            return new Vector(result);
         } else if (other instanceof Vector) {
-            double[] arr = new double[((Vector) other).VALUE.length];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = VALUE[i] - ((Vector) other).VALUE[i];
+            double[] result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] -= ((Vector) other).value[i];
             }
-            return new Vector(arr);
+            return new Vector(result);
         } else {
-            return super.sub(this);
+            return super.sub(other);
         }
     }
 
     @Override
     public Var mul(Var other) {
-
         if (other instanceof Scalar) {
-            double[] sub = Arrays.copyOf(VALUE, VALUE.length);
-            for (int i = 0; i < sub.length; i++) {
-                sub[i] *= ((Scalar) other).getVALUE();
+            double[] result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] *= ((Scalar) other).getValue();
             }
-            return new Vector(sub);
+            return new Vector(result);
         } else if (other instanceof Vector) {
-            double arr = 0;
-            for (int i = 0; i < VALUE.length; i++) {
-                arr += VALUE[i] * ((Vector) other).VALUE[i];
+            double[] result = Arrays.copyOf(value, value.length);
+            double sum = 0;
+            for (int i = 0; i < result.length; i++) {
+                result[i] *= ((Vector) other).value[i];
+                sum += result[i];
             }
-            return new Scalar(arr);
+            return new Scalar(sum);
         } else {
-            return super.mul(this);
+            return super.mul(other);
         }
     }
 
     @Override
     public Var div(Var other) {
         if (other instanceof Scalar) {
-            double[] sub = Arrays.copyOf(VALUE, VALUE.length);
-            for (int i = 0; i < sub.length; i++) {
-                sub[i] /= ((Scalar) other).getVALUE();
+            double[] result = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] /= ((Scalar) other).getValue();
             }
-            return new Vector(sub);
-
-        } else {
-            return super.div(this);
+            return new Vector(result);
         }
+        return super.div(other);
     }
 }
