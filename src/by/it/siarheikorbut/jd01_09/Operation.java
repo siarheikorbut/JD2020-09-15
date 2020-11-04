fcbf0f3b62174195511c5014ab8afa0148a9f2e9
siarheikorbut <siarheikorbut@gmail.com>
2020-11-04 21:38:59 +0300

    jd01_09 Task A, B, C completed

--- src/by/it/siarheikorbut/jd01_09/Operation.java
+++ src/by/it/siarheikorbut/jd01_09/Operation.java
@@ -1,5 +1,11 @@
 package by.it.siarheikorbut.jd01_09;
 
+/**
+ * @author Siarhei Korbut
+ * @see <a href="https://drive.google.com/file/d/1C-wHpUcHtxb-Qq0lfyExNQsYeKr3yIPh/view?usp=sharing">Задание JD01_09</a>
+ */
+
+
 interface Operation {
     Var add(Var other);
 
--- src/by/it/siarheikorbut/jd01_09/Scalar.java
+++ src/by/it/siarheikorbut/jd01_09/Scalar.java
@@ -2,8 +2,9 @@ package by.it.siarheikorbut.jd01_09;
 
 /**
  * @author Siarhei Korbut
- * @see <a href="https://drive.google.com/file/d/1C-wHpUcHtxb-Qq0lfyExNQsYeKr3yIPh/view?usp=sharing">Задание JD01_09 ( A )</a>
+ * @see <a href="https://drive.google.com/file/d/1C-wHpUcHtxb-Qq0lfyExNQsYeKr3yIPh/view?usp=sharing">Задание JD01_09</a>
  */
+
 
 class Scalar extends Var {
 
