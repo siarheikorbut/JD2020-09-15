d2f2e5a561a2c82577b1287e234c3a2bfd50faaa
siarheikorbut <siarheikorbut@gmail.com>
2020-11-05 21:57:09 +0300

    jd02_02 Task A, B, C completed

--- src/by/it/siarheikorbut/jd02_02/QueueCashier.java
+++ src/by/it/siarheikorbut/jd02_02/QueueCashier.java
@@ -7,24 +7,18 @@ public class QueueCashier {
 
     private QueueCashier() {
     }
-    private final static Deque<Cashier> QUEUE_CASHIER=new ArrayDeque<>();
 
-    static void add (Cashier cashier) {
+    private final static Deque<Cashier> QUEUE_CASHIER = new ArrayDeque<>();
+
+    static void add(Cashier cashier) {
         synchronized (QUEUE_CASHIER) {
             QUEUE_CASHIER.addLast(cashier);
         }
     }
-    static Cashier extract () {
+
+    static Cashier extract() {
         synchronized (QUEUE_CASHIER) {
             return QUEUE_CASHIER.pollFirst();
         }
     }
-
-//    public static int getSizeQUEUE_CASHIER() {
-//        return QUEUE_CASHIER.size();
-//    }
-
-//    public static Deque<Cashier> getQueueCashier() {
-//        return QUEUE_CASHIER;
-//    }
 }
